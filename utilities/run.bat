77a8006d22a1f26aecb10d20e741dc15254e4756
daminella <luca.daminato10@gmail.com>
2022-05-08 20:54:48 +0200

    Update Website
    Aggiornate pagine web

---
+++ utilities/run.bat
@@ -0,0 +1,8 @@
+cd ../app
+set FLASK_APP=app.py
+set FLASK_ENV=development
+set FLASK_KEY_PATH=../utilities/file_a_caso.txt
+set SERVER_HOST=home.dariomarizza.com
+set SERVER_PORT=42511
+
+flask run
