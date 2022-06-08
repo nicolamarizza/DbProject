30d6b7fe137eb34f96855b3b31fe2c74aac4ff4c
cogotti-giulia <cogotti.giulia.irl@gmail.com>
2022-06-08 18:44:08 +0200

    ho fatto il datetime carino per le lezioni

--- app/static/javascript/base.js
+++ app/static/javascript/base.js
@@ -1,1 +1,6 @@
-M.AutoInit();
+$(document).ready(function(){
+    M.AutoInit();
+    //inizializzatore per i datetime carini
+    MaterialDateTimePicker.create($('.datetimecool'))
+});
+
--- app/static/javascript/corsi.js
+++ app/static/javascript/corsi.js
@@ -34,21 +34,12 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.datepicker');
-  var instances = M.Datepicker.init(elems, {});
-});
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.timepicker');
-  var instances = M.Timepicker.init(elems, {});
-});
 
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems, {});
 });
-
 
 function resetForm() {
   document.getElementById("nuovo_corso_insert").reset();
