38588389b219fc06e3566a24140364736e13a5ec
cogotti-giulia <cogotti.giulia.irl@gmail.com>
2022-06-09 17:07:12 +0200

    fix datetime modifica corso

--- app/static/javascript/corso_elem.js
+++ app/static/javascript/corso_elem.js
@@ -1,4 +1,4 @@
-function hideOrShowFormModificaCorso(event){
+function hideOrShowFormModificaCorso(event, idcorso){
     divButton = event.target.parentNode.parentNode
     divParentForm = divButton.parentNode
     if(divParentForm.children[0].style.display === "none") {
@@ -6,22 +6,20 @@ function hideOrShowFormModificaCorso(event){
         divParentForm.children[1].style.display = "none"
         divButton.children[0].style.display = "block"
         divButton.children[1].style.display = "none"
+        
     } else {
         divParentForm.children[0].style.display = "none"
         divParentForm.children[1].style.display = "block"
         divButton.children[0].style.display = "none"
         divButton.children[1].style.display = "block"
+    
+        //datetime
+        MaterialDateTimePickerCorsi.create($('.inizioiscrizioni'+idcorso))
+        MaterialDateTimePickerCorsi.create($('.scadenzaiscrizioni'+idcorso))
+
     }
+
 }
 
 
-//elemento datetime material design
-$(document).ready(function(){
-    MaterialDateTimePickerCorsi.create($('.inizioiscrizioni'))
-  });    
-  
-  //elemento datetime material design
-  $(document).ready(function(){
-    MaterialDateTimePickerCorsi.create($('.scadenzaiscrizioni'))
-  });    
-  
+
