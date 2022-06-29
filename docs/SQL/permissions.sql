f1ad874649ea365058c2daabf60e406ad1d4cf0c
Nicola Marizza <nicola.marizza@gmail.com>
2022-06-29 12:53:00 +0200

    Docs

--- docs/SQL/permissions.sql
+++ docs/SQL/permissions.sql
@@ -10,16 +10,21 @@ grant all on corsi to docente;
 grant select on dipartimenti to docente;
 grant select on edifici to docente;
 grant select on iscrizioni_corsi to docente;
-grant select on responsabili_corsi to docente;
 grant all on lezioni to docente;
 grant select on prenotazioni_lezioni to docente;
+grant all on responsabili_corsi to docente;
+grant select, update on utenti to docente;
 
+grant update on sequence lezioni_id_seq to docente;
+grant update on sequence corsi_id_seq to docente;
 
 grant select on aule to studente;
 grant select on categorie to studente;
 grant select on corsi to studente;
 grant select on dipartimenti to studente;
 grant select on edifici to studente;
-grant select, insert on iscrizioni_corsi to studente;
-grant select, insert on prenotazioni_lezioni to studente;
+grant select on iscrizioni_corsi to studente;
 grant select on lezioni to studente;
+grant select, insert on prenotazioni_lezioni to studente;
+grant select on responsabili_corsi to studente;
+grant select, update on utenti to studente;
